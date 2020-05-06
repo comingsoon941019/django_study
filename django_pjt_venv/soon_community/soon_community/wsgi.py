e85caa5813811304c6a57f6aea44a0eed00227ae
comingsoon <comingsoon941019@gmail.com>
2020-05-06 10:12:26 +0900

    django study start

---
+++ django_pjt_venv/soon_community/soon_community/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for soon_community project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soon_community.settings')
+
+application = get_wsgi_application()
